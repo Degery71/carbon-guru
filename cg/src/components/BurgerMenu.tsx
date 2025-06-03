1ce6aa4cca3d39399715166d69c9f83dbd28d47d
Diego Alejandro Ortega Manzano <diegoortegamanzano@gmail.com>
2025-06-03 16:27:12 +0200

    fully working burger menu

--- cg/src/components/BurgerMenu.tsx
+++ cg/src/components/BurgerMenu.tsx
@@ -11,31 +11,52 @@ export const BurgerMenu = () => {
   const toggleNavbar = () => {
     setIsOpen(!isOpen);
   };
+  // anchors to different parts of the text
+  const links = ["Link", "Link2", "Link3"];
+
   return (
-    <div className="h-16 min-h-full flex justify-around content-center border-2 border-x-0 bg-pal-dark sticky top-0 z-20">
-      {/* Burger Logo */}
-      <div className="flex items-center justify-center" onClick={toggleNavbar}>
-        {!isOpen ? (
-          <GiHamburgerMenu size={32} color="white" className="align-middle" />
-        ) : (
-          <IoCloseSharp size={32} color="white" className="align-middle" />
-        )}
+    <div className="flex flex-col">
+      <div className="h-16 min-h-full flex justify-around content-center border-b-2 bg-pal-dark sticky top-0 z-20">
+        {/* Burger Logo */}
+        <div
+          className="flex items-center justify-center"
+          onClick={toggleNavbar}
+        >
+          {!isOpen ? (
+            <GiHamburgerMenu size={32} color="white" className="align-middle" />
+          ) : (
+            <IoCloseSharp size={32} color="white" className="align-middle" />
+          )}
+        </div>
+
+        <Link
+          className="[font-family:'Coda_Caption-ExtraBold',Helvetica] font-extrabold text-white text-[28px] content-center"
+          href="/"
+        >
+          {" "}
+          CARBÓN GURU
+        </Link>
+
+        <Link
+          className="flex items-center justify-center"
+          href="https://www.instagram.com/_carbonguru_/"
+        >
+          <FaInstagram size={28} color="white" className="align-middle" />
+        </Link>
       </div>
-
-      <Link
-        className="[font-family:'Coda_Caption-ExtraBold',Helvetica] font-extrabold text-white text-[28px] content-center"
-        href="/"
-      >
-        {" "}
-        CARBÓN GURU
-      </Link>
-
-      <Link
-        className="flex items-center justify-center"
-        href="https://www.instagram.com/_carbonguru_/"
-      >
-        <FaInstagram size={28} color="white" className="align-middle" />
-      </Link>
+      {isOpen && (
+        <div className="flex flex-col justify-center content-center text-center">
+          {links.map((link: string) => (
+            <Link
+              key={link}
+              className="[font-family:'Coda_Caption-ExtraBold',Helvetica] font-extrabold text-white text-[28px] border-b-2 border-black"
+              href={`#${link}`}
+            >
+              {link}
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
