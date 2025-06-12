821d3a89307a6dd9d5fb504a2454e03305ea04ef
Diego Alejandro Ortega Manzano <diegoortegamanzano@gmail.com>
2025-06-12 16:37:43 +0200

    fixing styles on itemlist

--- cg/src/app/carta/pinchos/page.tsx
+++ cg/src/app/carta/pinchos/page.tsx
@@ -7,6 +7,7 @@ export default function Card() {
       src: "/pincho.jpg",
       alt: "PINCHO DE POLLO/ CHICKEN SKEWER",
       item: "PINCHO DE POLLO/ CHICKEN SKEWER",
+      price: "2,00€",
       description:
         "Pechuga de pollo a la brasa, cortada en tiras, marinada con nuestra salsa especial, acompañada con papa cocida/ grilled chicken breast cut into strips, marinated with our especial sause, accompanied with bolied potatoes.",
     },
@@ -14,6 +15,7 @@ export default function Card() {
       src: "/pincho1.jpg",
       alt: "PINCHO DE CERDO/ PORK SKEWER",
       item: "PINCHO DE CERDO/ PORK SKEWER",
+      price: "2,00€",
       description:
         "Bondiola de cerdo a la brasa, cortada en tiras, marinada con nuestra salsa especial, acompañado con papa cocida/ grilled pork bondiola cut into strips marinated with our special sauce, accompanied with boiled potatoes.",
     },
@@ -21,6 +23,7 @@ export default function Card() {
       src: "/pincho.jpg",
       alt: "PINCHO DE CALABACÍN/ ZUCCHINI SKEWER",
       item: "PINCHO DE CALABACÍN/ ZUCCHINI SKEWER",
+      price: "2,00€",
       description:
         "Calabacin a la brasa cortado en rodajas, marinado con nuestra salsa especial, acompañado con papa cocida/ grilled zucchini cut into slices marinated with our special sauce, accompanied by boiled potatoes",
     },
@@ -28,6 +31,7 @@ export default function Card() {
       src: "/pincho1.jpg",
       alt: "PINCHO DE BERENJENA/ EGGLANT SKEWER",
       item: "PINCHO DE BERENJENA/ EGGLANT SKEWER",
+      price: "2,00€",
       description:
         "Berenjena a la brasa cortada en rodajas, marinada con nuestra salsa especial, compañada con papa cocida/ grilled eggplant cut into slices marinated with our special sauce, accompanied by boliled potatoes.",
     },
@@ -35,6 +39,7 @@ export default function Card() {
       src: "/pincho.jpg",
       alt: "PINCHO DE PIMIENTO Y CEBOLLA/ PEPPER AND ONION SKEWER",
       item: "PINCHO DE PIMIENTO Y CEBOLLA/ PEPPER AND ONION SKEWER",
+      price: "2,00€",
       description:
         "Pimiento y Cebolla a la brasa, marinado con nuestra salsa especial, acompañado con papa cocida/grilled pepper and onion marinated with our special sauce, accompanied by boiled potatoes.",
     },
@@ -42,6 +47,7 @@ export default function Card() {
       src: "/pincho1.jpg",
       alt: "PINCHO DE TERNERA/ BEEF SKEWER",
       item: "PINCHO DE TERNERA/ BEEF SKEWER",
+      price: "2,70€",
       description:
         "Ternera a la brasa, cortada en tiras, marinada con nuestra salsa especial, acompañada con papa cocida/ grilled beef cut into strips marinated with our special sauce, accompanied with bolied potatoes.",
     },
@@ -49,6 +55,7 @@ export default function Card() {
       src: "/pincho.jpg",
       alt: "PINCHO MIXTO/ MIXED SKEWER",
       item: "PINCHO MIXTO/ MIXED SKEWER",
+      price: "2,80€",
       description:
         "La mejor combinación de ternera y pollo a la brasa, marinados con nuestra salsa especial y acompañados con papa cocida/ the best combination of grilled beef and chicken, marinated with our special sauce, accompanied with boiled potatoes.",
     },
@@ -56,6 +63,7 @@ export default function Card() {
       src: "/pincho1.jpg",
       alt: "PINCHO DE CHORIZO A LA BRASA/GRILLED CHORIZO SKEWER",
       item: "PINCHO DE CHORIZO A LA BRASA/GRILLED CHORIZO SKEWER",
+      price: "2,90€",
       description:
         "Delicioso pincho casero a la brasa/delicious homemade grilled skewers.",
     },
--- cg/src/components/MenuItemList.tsx
+++ cg/src/components/MenuItemList.tsx
@@ -4,6 +4,7 @@ type MenuItem = {
   src: string;
   alt: string;
   item: string;
+  price: string;
   description: string;
 };
 
@@ -13,19 +14,24 @@ type Props = {
 
 export const MenuItemList = ({ items }: Props) => {
   return (
-    <div>
+    <div className="">
       {items.map((menuItem, index) => (
-        <div key={index} className="mb-4">
+        <div key={index} className="m-4">
           <Image
             src={menuItem.src}
             width={400}
             height={400}
             alt={menuItem.alt}
           />
-          <div className="font-extrabold text-white text-xl items-start">
-            {menuItem.item}
+          <div className="flex justify-between">
+            <div className="font-extrabold text-white text-xl">
+              {menuItem.item}
+            </div>
+            <div className="font-extrabold text-white text-xl">
+              {menuItem.price}
+            </div>
           </div>
-          <div className="font-extrabold text-white text-s items-start">
+          <div className="font-extrabold text-white text-s">
             {menuItem.description}
           </div>
         </div>
